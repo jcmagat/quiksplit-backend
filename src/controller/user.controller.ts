3b9f0a56308248a0fce21e9615871788386f13b1
Juan Carlo Magat <magatjuancarlo@gmail.com>
2023-06-23 10:13:22 -0700

    Create User entity, controller, and router

---
+++ src/controller/user.controller.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { AppDataSource } from "../data-source.js";
+import { User } from "../entity/user.entity.js";
+
+const getUsers = async (req: Request, res: Response) => {
+  const users = await AppDataSource.getRepository(User).find();
+  res.send(users);
+};
+
+const createUser = async (req: Request, res: Response) => {
+  const { username } = req.body;
+  const newUser = { username };
+
+  const results = await AppDataSource.createQueryBuilder()
+    .insert()
+    .into(User)
+    .values(newUser)
+    .returning("*")
+    .execute();
+
+  return res.send(results.raw[0]);
+};
+
+export default { getUsers, createUser };
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
-import fs from "fs";
+import "reflect-metadata";
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import { AppDataSource } from "./data-source.js";
+import userRouter from "./router/user.router.js";
 
-console.log("hello world!");
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Data Source has been initialized!");
+  })
+  .catch((err) =>
+    console.error("Error during Data Source initialization:", err)
+  );
+
+app.use(express.json());
+
+// ========== Routes ==========
+app.use("/users", userRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
